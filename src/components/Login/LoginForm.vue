9874d23b69962d87ece4875cf1ab6d7aeb23507b
lee <unknown9732@naver.com>
2022-02-13 13:38:24 +0900

    refactor(LoginForm.vue): div -> section 태그 수정

--- src/components/Login/LoginForm.vue
+++ src/components/Login/LoginForm.vue
@@ -1,28 +1,28 @@
 <template>
   <form class="absolute flex h-full w-full flex-col items-center justify-center">
-    <div class="mt-1 mb-1">
+    <section class="mt-1 mb-1">
       <label for="user-id">
         <input id="user-id" type="text" placeholder="아이디" data-test="input-id" v-model="id">
       </label>
-    </div>
-    <div class="mt-1 mb-1">
+    </section>
+    <section class="mt-1 mb-1">
       <label for="user-password">
         <input id="user-password" type="password" placeholder="패스워드" data-test="input-password"
                v-model="password">
       </label>
-    </div>
+    </section>
     <button class="bg-blue-400 hover:bg-blue-500 px-4 py-2 mt-1 mb-1 text-white font-bold rounded"
             type="button">
       로그인
     </button>
-    <div>
+    <section>
       <span>아이디:</span>
       <span data-test="show-id">{{ id }}</span>
-    </div>
-    <div>
+    </section>
+    <section>
       <span>패스워드:</span>
       <span data-test="show-password">{{ password }}</span>
-    </div>
+    </section>
   </form>
 </template>
 
