612d8d44839928f32790e9ae0cd187b2f53e3915
lee <unknown9732@naver.com>
2022-02-13 10:38:57 +0900

    feat(LoginForm.vue): 테스트 성공 코드 작성

---
+++ src/components/Login/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form>
+      <label for="user-id">아이디
+        <input id="user-id" type="text" data-test="input-id" v-model="id">
+      </label>
+
+      <label for="user-password">패스워드
+        <input id="user-password" type="password" data-test="input-password" v-model="password">
+      </label>
+    </form>
+  </div>
+
+  <div>
+    <p data-test="show-id">{{ id }}</p>
+    <p data-test="show-password">{{ password }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      id: '',
+      password: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
