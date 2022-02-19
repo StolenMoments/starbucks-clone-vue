aac6b5a66df3ea0718fed2e6f6349b2f1223b9be
lee <unknown9732@naver.com>
2022-02-19 15:23:23 +0900

    refactor(MenuFooterBar.vue): 최상단 section 제외한 section -> div 태그 수정

--- src/components/Menu/MenuFooterBar.vue
+++ src/components/Menu/MenuFooterBar.vue
@@ -1,18 +1,18 @@
 <template>
-  <footer class="fixed flex bottom-0 w-full border-t-2">
-    <section class="w-full bg-gray-200 text-center">
+  <section class="fixed flex bottom-0 w-full border-t-2">
+    <div class="w-full bg-gray-200 text-center">
       <HomeIcon class="h-5 w-5 m-auto"/>
       <span>홈</span>
-    </section>
-    <section class="w-full bg-gray-200 text-center">
+    </div>
+    <div class="w-full bg-gray-200 text-center">
       <ShoppingCartIcon class="h-5 w-5 m-auto"/>
       <span>장바구니</span>
-    </section>
-    <section class="w-full bg-gray-200 text-center">
+    </div>
+    <div class="w-full bg-gray-200 text-center">
       <CogIcon class="h-5 w-5 m-auto"/>
       <span>설정</span>
-    </section>
-  </footer>
+    </div>
+  </section>
 </template>
 
 <script>
