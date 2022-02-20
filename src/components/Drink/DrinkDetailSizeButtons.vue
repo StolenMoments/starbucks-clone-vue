6b61fc849c1a22f60f5e533ab175a26222641e2b
lee <unknown9732@naver.com>
2022-02-20 23:14:13 +0900

    test(DrinkDetailSizeButtons): 테스트를 위한 :data-test 애트리뷰트 추가

--- src/components/Drink/DrinkDetailSizeButtons.vue
+++ src/components/Drink/DrinkDetailSizeButtons.vue
@@ -3,10 +3,13 @@
   <div class="flex">
     <div v-for="size in sizeList" :key="size">
       <button v-if="sizeObject[size]"
-              class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300">{{ size }}
+              class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300"
+              :data-test="`${size}`"
+      >{{ size }}
       </button>
       <button v-else
-              class="h-10 w-24 cursor-auto rounded border-2 text-gray-200">{{ size }}
+              class="h-10 w-24 cursor-auto rounded border-2 text-gray-200"
+              :data-test="`${size}`">{{ size }}
       </button>
     </div>
   </div>
