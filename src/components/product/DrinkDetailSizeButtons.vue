d39a5745b019d1b0da1d8706c9057339e7581004
lee <unknown9732@naver.com>
2022-02-20 22:44:45 +0900

    refactor(DrinkDetailSizeButtons): v-for 활용으로 button 간소화

--- src/components/product/DrinkDetailSizeButtons.vue
+++ src/components/product/DrinkDetailSizeButtons.vue
@@ -1,30 +1,14 @@
 <template>
   <p class="font-bold">사이즈</p>
-  <div class="flex place-content-between">
-    <button v-if="drinkSize.Short"
-            class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300">Short
-    </button>
-    <button v-else
-            class="h-10 w-24 cursor-auto rounded border-2 text-gray-200">Short
-    </button>
-    <button v-if="drinkSize.Tall"
-            class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300">Tall
-    </button>
-    <button v-else
-            class="h-10 w-24 cursor-auto rounded border-2 text-gray-200">Tall
-    </button>
-    <button v-if="drinkSize.Grande"
-            class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300">Grande
-    </button>
-    <button v-else
-            class="h-10 w-24 cursor-auto rounded border-2 text-gray-200">Grande
-    </button>
-    <button v-if="drinkSize.Venti"
-            class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300">Venti
-    </button>
-    <button v-else
-            class="h-10 w-24 cursor-auto rounded border-2 text-gray-200">Venti
-    </button>
+  <div class="flex">
+    <div v-for="size in sizeList" :key="size">
+      <button v-if="sizeObject[size]"
+              class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300">{{ size }}
+      </button>
+      <button v-else
+              class="h-10 w-24 cursor-auto rounded border-2 text-gray-200">{{ size }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -32,7 +16,8 @@
 export default {
   name: 'DrinkDetailSizeButtons',
   props: {
-    drinkSize: Object,
+    sizeObject: Object,
+    sizeList: Array,
   },
 };
 </script>
