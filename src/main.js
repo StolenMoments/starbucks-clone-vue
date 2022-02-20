ff140f3bb76f3e3ff54c1bc5c484d13b6d2d8fa2
lee <unknown9732@naver.com>
2022-02-21 00:12:24 +0900

    chore(main.js): FontAwesomeIcon 사용을 위한 설정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import App from './App.vue';
 import './index.css';
 
-createApp(App).mount('#app');
+library.add(fas, far);
+const app = createApp(App);
+app.component('FasIcon', FontAwesomeIcon);
+app.mount('#app');
