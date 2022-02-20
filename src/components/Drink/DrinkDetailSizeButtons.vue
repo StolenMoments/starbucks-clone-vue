910371cdb7f30a77b048c3fb0328410cc3329de8
lee <unknown9732@naver.com>
2022-02-21 00:13:21 +0900

    style(DrinkDetailSizeButtons): 사이즈 버튼 아이콘 추가 및 스타일링

--- src/components/Drink/DrinkDetailSizeButtons.vue
+++ src/components/Drink/DrinkDetailSizeButtons.vue
@@ -3,9 +3,18 @@
   <div class="flex">
     <div v-for="size in sizeList" :key="size">
       <button v-if="sizeObject[size]"
-              class="h-10 w-24 cursor-pointer rounded border-2 hover:bg-green-300"
+              class="h-12 w-24 cursor-pointer rounded border-2 hover:bg-green-300"
               :data-test="`${size}`"
-      >{{ size }}
+      >
+        <FasIcon v-if="size === 'Short'"
+                 class="h-3 w-3" :icon="['fas','mug-hot']"/>
+        <FasIcon v-if="size === 'Tall'"
+                 class="h-3.5 w-3.5" :icon="['fas','mug-hot']"/>
+        <FasIcon v-if="size === 'Grande'"
+                 class="h-4 w-4" :icon="['fas','mug-hot']"/>
+        <FasIcon v-if="size === 'Venti'"
+                 class="h-5 w-5" :icon="['fas','mug-hot']"/>
+        {{ size }}
       </button>
       <button v-else
               class="h-10 w-24 cursor-auto rounded border-2 text-gray-200"
