8c74074aebe99d7d79915f240781adb8eecde63c
lee <unknown9732@naver.com>
2022-02-26 18:15:00 +0900

    test(ProductList): 빠뜨린 text() 메서드 호출

--- tests/unit/ProductList.spec.js
+++ tests/unit/ProductList.spec.js
@@ -13,7 +13,7 @@ describe('렌더링 테스트', () => {
             isNew: false,
             name: '카라멜 마끼아또',
             engName: 'Caramel Macchiato',
-            price: '5000',
+            price: 5000,
             desc: '향긋한 바닐라 시럽과 따뜻한 스팀 밀크 위에 풍성한 우유 거품을 얹고 점을 찍듯이 에스프레소를 부은 후 벌집 모양으로 카라멜 드리즐을 올린 달콤한 커피 음료',
           },
           {
@@ -22,7 +22,7 @@ describe('렌더링 테스트', () => {
             isNew: false,
             name: '카푸치노',
             engName: 'Cappuccino',
-            price: '4500',
+            price: 4500,
             desc: '풍부하고 진한 에스프레소에 따뜻한 우유와 벨벳 같은 우유 거품이 1:1 비율로 어우러져 마무리된 커피 음료',
           },
           {
@@ -31,7 +31,7 @@ describe('렌더링 테스트', () => {
             isNew: true,
             name: '슈크림 라떼',
             engName: 'Choux Cream Latte',
-            price: '5500',
+            price: 5500,
             desc: '오리지널 슈크림의 맛을 그대로 재현한,\nChoux is Coming!\n봄을 알리는 스타벅스의 시그니처 음료 슈크림 라떼!\n퍼스널 옵션을 선택하여 블론드/디카페인 원두로 즐겨보세요.',
           },
         ],
@@ -40,38 +40,38 @@ describe('렌더링 테스트', () => {
   });
 
   test('카테고리 렌더링', () => {
-    expect(wrapper.get('[data-test="category-name"'))
+    expect(wrapper.get('[data-test="category-name"').text())
       .toBe('에스프레소');
   });
 
   test('음료명 한글 렌더링', () => {
     const productNames = wrapper.findAll('[data-test="product-name"');
 
-    expect(productNames.at(0)).toBe('카라멜 마끼아또');
-    expect(productNames.at(1)).toBe('카푸치노');
-    expect(productNames.at(2)).toBe('슈크림 라떼');
+    expect(productNames.at(0).text()).toBe('카라멜 마끼아또');
+    expect(productNames.at(1).text()).toBe('카푸치노');
+    expect(productNames.at(2).text()).toBe('슈크림 라떼');
   });
 
   test('음료명 영문 렌더링', () => {
     const productEngNames = wrapper.findAll('[data-test="product-eng-name"');
 
-    expect(productEngNames.at(0)).toBe('Caramel Macchiato');
-    expect(productEngNames.at(1)).toBe('Cappuccino');
-    expect(productEngNames.at(2)).toBe('Choux Cream Latte');
+    expect(productEngNames.at(0).text()).toBe('Caramel Macchiato');
+    expect(productEngNames.at(1).text()).toBe('Cappuccino');
+    expect(productEngNames.at(2).text()).toBe('Choux Cream Latte');
   });
 
   test('가격 렌더링', () => {
     const productPrices = wrapper.findAll('[data-test="product-price"');
 
-    expect(productPrices.at(0)).toBe('5,000원');
-    expect(productPrices.at(1)).toBe('4,500원');
-    expect(productPrices.at(2)).toBe('5,500원');
+    expect(productPrices.at(0).text()).toBe('5,000원');
+    expect(productPrices.at(1).text()).toBe('4,500원');
+    expect(productPrices.at(2).text()).toBe('5,500원');
   });
 
   test('위첨자 렌더링', () => {
     const productSuperscripts = wrapper.findAll('[data-test="product-superscript"');
 
-    expect(productSuperscripts.at(0)).toBe('Best');
-    expect(productSuperscripts.at(1)).toBe('New');
+    expect(productSuperscripts.at(0).text()).toBe('Best');
+    expect(productSuperscripts.at(1).text()).toBe('New');
   });
 });
