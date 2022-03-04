6aee3b6a0fbb40c5753d5704052624e2ee0f7429
lee <unknown9732@naver.com>
2022-03-04 20:06:06 +0900

    feat(main.js) : route 선언

--- src/main.js
+++ src/main.js
@@ -3,10 +3,49 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import App from './App.vue';
 import './index.css';
+import LoginForm from '@/components/Login/LoginForm.vue';
+import ProductDetail from '@/components/Product/ProductDetail.vue';
+import ProductList from '@/components/Product/ProductList.vue';
+import OrderCart from '@/components/Order/OrderCart.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import App from './App.vue';
+
+const login = LoginForm;
+const productDetail = ProductDetail;
+const productList = ProductList;
+const orderCart = OrderCart;
+
+const routes = [
+  {
+    path: '/',
+    component: login,
+    name: 'login',
+  },
+  {
+    path: '/product',
+    component: productList,
+    name: 'list',
+  },
+  {
+    path: '/product/:id',
+    component: productDetail,
+    name: 'detail',
+  },
+  {
+    path: '/cart',
+    component: orderCart,
+    name: 'cart',
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
 
 library.add(fas, far);
 const app = createApp(App);
+app.use(router);
 app.component('FasIcon', FontAwesomeIcon);
 app.mount('#app');
