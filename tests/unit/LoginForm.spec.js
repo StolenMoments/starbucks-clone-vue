b3fc8867ba05074784de96ae46f6ff3ed8d17689
lee <unknown9732@naver.com>
2022-03-05 11:52:25 +0900

    test(LoginForm) : 테스트 항목 추가

--- tests/unit/LoginForm.spec.js
+++ tests/unit/LoginForm.spec.js
@@ -1,22 +1,85 @@
 import { mount } from '@vue/test-utils';
 import LoginForm from '../../src/components/Login/LoginForm.vue';
 
-test('입력한 아이디를 보여준다.', async () => {
+describe('렌더링 테스트', () => {
   const wrapper = mount(LoginForm);
-  const inputId = wrapper.get('[data-test="input-id"]');
-  const showId = wrapper.get('[data-test="show-id"]');
+  test('아이디 입력 부분 렌더링', () => {
+    expect(wrapper.get('[data-test="input-username"]')
+      .attributes('placeholder'))
+      .toBe('아이디');
+  });
 
-  await inputId.setValue('user1234');
-  expect(showId.text())
-    .toBe('user1234');
+  test('패스워드 입력 부분 렌더링', () => {
+    expect(wrapper.get('[data-test="input-password"]')
+      .attributes('placeholder'))
+      .toBe('패스워드');
+  });
+
+  test('로그인 버튼 렌더링', () => {
+    expect(wrapper.get('[data-test="login-button"]')
+      .text())
+      .toBe('로그인');
+  });
 });
 
-test('입력한 패스워드를 보여준다.', async () => {
-  const wrapper = mount(LoginForm);
-  const inputPassword = wrapper.get('[data-test="input-password"]');
-  const showPassword = wrapper.get('[data-test="show-password"]');
+describe('기능 테스트', () => {
+  const mockRoute = {
+    params: {
+      id: 1,
+    },
+  };
+  const mockRouter = {
+    push: jest.fn(),
+  };
 
-  await inputPassword.setValue('password1234');
-  expect(showPassword.text())
-    .toBe('password1234');
+  const wrapper = mount(LoginForm, {
+    global: {
+      mocks: {
+        $route: mockRoute,
+        $router: mockRouter,
+      },
+    },
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('입력한 아이디를 보여준다.', async () => {
+    const inputUsername = wrapper.get('[data-test="input-username"]');
+    const showId = wrapper.get('[data-test="show-id"]');
+
+    await inputUsername.setValue('user1234');
+    expect(showId.text())
+      .toBe('user1234');
+  });
+
+  test('입력한 패스워드를 보여준다.', async () => {
+    const inputPassword = wrapper.get('[data-test="input-password"]');
+    const showPassword = wrapper.get('[data-test="show-password"]');
+
+    await inputPassword.setValue('password1234');
+    expect(showPassword.text())
+      .toBe('password1234');
+  });
+
+  test('로그인 성공 테스트', async () => {
+    const inputUsername = wrapper.get('[data-test="input-username"]');
+    const inputPassword = wrapper.get('[data-test="input-password"]');
+    const loginButton = wrapper.get('[data-test="login-button"]');
+
+    await inputUsername.setValue('testid');
+    await inputPassword.setValue('testpassword');
+    await loginButton.trigger('click');
+  });
+
+  test('로그인 실패 테스트', async () => {
+    const inputUsername = wrapper.get('[data-test="input-username"]');
+    const inputPassword = wrapper.get('[data-test="input-password"]');
+    const loginButton = wrapper.get('[data-test="login-button"]');
+
+    await inputUsername.setValue('failid');
+    await inputPassword.setValue('failpassword');
+    await loginButton.trigger('click');
+  });
 });
