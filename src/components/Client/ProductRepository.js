36cb77b1a083fee9f93b3a9a320cb1444e90bb0d
lee <unknown9732@naver.com>
2022-03-06 18:22:13 +0900

    feat(ProductRepository) : axios 헤더 설정값 추가

--- src/components/Client/ProductRepository.js
+++ src/components/Client/ProductRepository.js
@@ -3,13 +3,17 @@ import Repository from '@/components/Client/Repository';
 const resource = '/product';
 
 export default class ProductRepository extends Repository {
+  getProducts() {
+    return this.client.get(`${resource}`, this.getAuthHeader());
+  }
+
   getProductsByCategory(payload) {
     const { category } = payload;
-    return this.client.get(`${resource}?category=${category}`);
+    return this.client.get(`${resource}?category=${category}`, this.getAuthHeader());
   }
 
   getProduct(payload) {
     const { productNo } = payload;
-    return this.client.get(`${resource}/${productNo}`);
+    return this.client.get(`${resource}/${productNo}`, this.getAuthHeader());
   }
 }
