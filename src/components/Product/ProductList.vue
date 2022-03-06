9addcd60cfe905070a2cd137c3dc71503640c3bb
lee <unknown9732@naver.com>
2022-03-06 18:24:23 +0900

    feat(ProductList) : API 명세에 맞춰 수정

--- src/components/Product/ProductList.vue
+++ src/components/Product/ProductList.vue
@@ -3,18 +3,18 @@
     <section class="mt-8 ml-6">
       <span class="font-bold text-2xl" data-test="category-name">{{ categoryName }}</span>
     </section>
-    <section v-for="product in products" :key="product.id" class="ProductSection">
-      <img alt="product image" :src="product.img"
+    <section v-for="(product, index) in products" :key="index" class="ProductSection">
+      <img alt="product image" :src="product.imgUrl"
            class="ProductImg" data-test="product-img"/>
       <div class="ProductText">
         <div>
-          <span class="font-bold" data-test="product-name">{{ product.name }}</span>
+          <span class="font-bold" data-test="product-name-kr">{{ product.nameKr }}</span>
           <ProductSuperscript v-if="product.isHot" type="Hot" data-test="superscript-hot"/>
-          <ProductSuperscript v-if="product.isNew" type="New" data-test="superscript-new"/>
+          <ProductSuperscript v-if="product.isNewProduct" type="New" data-test="superscript-new"/>
         </div>
         <p class="text-gray-400 overflow-hidden text-ellipsis whitespace-nowrap"
-           data-test="product-eng-name">
-          {{ product.engName }}
+           data-test="product-name-eng">
+          {{ product.nameEng }}
         </p>
         <p data-test="product-price">{{ addComma(product.price) }}</p>
       </div>
@@ -24,6 +24,9 @@
 
 <script>
 import ProductSuperscript from '@/components/Product/ProductSuperscript.vue';
+import ProductRepository from '@/components/Client/ProductRepository';
+
+const productRepository = new ProductRepository();
 
 export default {
   name: 'ProductList',
@@ -31,68 +34,17 @@ export default {
   data() {
     return {
       categoryName: '콜드 브루',
-      products: [
-        {
-          id: 1,
-          img: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000003285]_20210416154437226.jpg',
-          isHot: false,
-          isNew: false,
-          name: '콜드 브루 오트 라떼',
-          engName: 'Cold Brew With Oat Milk',
-          price: 5800,
-        },
-        {
-          id: 2,
-          img: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002081]_20210415133657016.jpg',
-          isHot: false,
-          isNew: true,
-          name: '돌체 콜드 브루',
-          engName: 'Dolce Cold Brew',
-          price: 6000,
-        },
-        {
-          id: 3,
-          img: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000487]_20210430112319174.jpg',
-          isHot: true,
-          isNew: true,
-          name: '바닐라 크림 콜드 브루',
-          engName: 'Vanilla Cream Cold Brew',
-          price: 5800,
-        },
-        {
-          id: 4,
-          img: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000479]_20210426091844065.jpg',
-          isHot: true,
-          isNew: false,
-          name: '나이트로 콜드 브루',
-          engName: 'Nitro Cold Brew',
-          price: 6800,
-        },
-        {
-          id: 5,
-          img: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000038]_20210430113202595.jpg',
-          isHot: false,
-          isNew: false,
-          name: '콜드 브루',
-          engName: 'Cold Brew',
-          price: 4500,
-        },
-        {
-          id: 6,
-          img: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/08/[9200000003661]_20210819094346334.jpg',
-          isHot: true,
-          isNew: false,
-          name: '시그니처 더 블랙 콜드 브루',
-          engName: 'Signature The Black Cold Brew',
-          price: 7500,
-        },
-      ],
+      products: [],
     };
   },
   methods: {
     addComma(price) {
       return price ? `${price.toLocaleString('ko-KR')}원` : price;
     },
+  },
+  async mounted() {
+    const response = await productRepository.getProducts();
+    this.$data.products = response.data.products;
   },
 };
 </script>
