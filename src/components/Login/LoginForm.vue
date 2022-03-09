47cec69e6e3c7a270a8ef7eb83a1ed018523899f
lee <unknown9732@naver.com>
2022-03-09 13:32:32 +0900

    style(LoginForm) : 로그인 실패 모달창 스타일 수정

--- src/components/Login/LoginForm.vue
+++ src/components/Login/LoginForm.vue
@@ -34,11 +34,13 @@
       <span data-test="show-password">{{ password }}</span>
     </section>
   </form>
-  <section v-if="isLoginFail" class="ModalBlock" @click="toggleModal" @keydown="toggleModal">
-    <div class="ModalContent">
+  <section v-if="isLoginFail" class="flex items-center justify-center">
+    <section class="ModalBlock" @click="toggleModal" @keydown="toggleModal">
+    </section>
+    <section class="ModalContent">
       <div class="text-3xl font-bold">로그인에 실패했습니다</div>
       <div class="mt-4 text-md font-bold">화면 클릭, 스페이스바 / 엔터키를 누르면 창이 닫힙니다.</div>
-    </div>
+    </section>
   </section>
 </template>
 
@@ -92,11 +94,18 @@ export default {
 }
 
 .ModalBlock {
-  @apply absolute h-full w-full flex justify-center items-center top-0 bg-gray-200
+  @apply fixed h-full w-full flex justify-center items-center top-0 bg-gray-200 opacity-50 z-0
 }
 
 .ModalContent {
-  @apply absolute h-1/4 w-1/2 flex flex-col justify-center items-center bg-white
+  @apply absolute inline-block top-72 h-1/5 w-1/2 flex flex-col justify-center
+  items-center z-50 bg-white text-center
+}
+
+@media (max-width: 425px) {
+  .ModalContent {
+    @apply w-full
+  }
 }
 
 </style>
