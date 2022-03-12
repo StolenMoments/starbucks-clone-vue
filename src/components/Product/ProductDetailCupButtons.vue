d4e5ab7693665e7e1d7bd689e6d36ab3565d5528
lee <unknown9732@naver.com>
2022-03-13 02:59:32 +0900

    style(ProductDetail) : 컵 종류 버튼 스타일 롤백

--- src/components/Product/ProductDetailCupButtons.vue
+++ src/components/Product/ProductDetailCupButtons.vue
@@ -1,18 +1,18 @@
 <template>
   <p class="font-bold">컵 선택</p>
-  <button class="Cup"
+  <button class="h-10 w-1/3 cursor-pointer rounded-l-2xl border-2 hover:bg-green-300"
           :class="{'bg-green-500 font-bold text-white': selected === 'storeCup'}"
           @click="selectCup('storeCup')"
           data-test="store-cup"
   >매장컵
   </button>
-  <button class="Cup"
+  <button class="h-10 w-1/3 cursor-pointer rounded border-2 hover:bg-green-300"
           :class="{'bg-green-500 font-bold text-white': selected === 'personalCup'}"
           @click="selectCup('personalCup')"
           data-test="personal-cup"
   >개인컵
   </button>
-  <button class="Cup"
+  <button class="h-10 w-1/3 cursor-pointer rounded-r-2xl border-2 hover:bg-green-300"
           @click="selectCup('disposableCup')"
           :class="{'bg-green-500 font-bold text-white': selected === 'disposableCup'}"
           data-test="disposable-cup"
@@ -36,9 +36,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.Cup {
-  @apply h-10 w-1/3 cursor-pointer rounded-l-2xl border-2 hover:bg-green-300
-}
-
-</style>
+<style scoped></style>
