fd2459dcaf2016aec648ad4e8cbaf55e958313a5
lee <unknown9732@naver.com>
2022-03-14 02:12:47 +0900

    feat(store) : 장바구니 총 금액 계산하는 getter 추가

--- src/store.js
+++ src/store.js
@@ -3,11 +3,33 @@ import { createStore } from 'vuex';
 export default createStore({
   state: {
     token: sessionStorage.getItem('token') || '',
-    cart: [],
+    cart: JSON.parse(sessionStorage.getItem('cart')) || [],
   },
   getters: {
-    getToken() {
-      return this.state.token;
+    getToken(state) {
+      return state.token;
+    },
+    getTotalPrice(state) {
+      function getProductPrice(item) {
+        if (!item) return 0;
+        let price = item.product.price * item.quantity;
+        const {
+          optionsInfo,
+          options,
+        } = item;
+        for (let i = 0; i < optionsInfo.length; i += 1) {
+          price += (optionsInfo[i].unitprice * (options[i].quantity - optionsInfo[i].baseQuantity));
+        }
+        return price;
+      }
+
+      let totalPrice = 0;
+      if (state.cart.length === 0) return 0;
+      for (let i = 0; i < state.cart.length; i += 1) {
+        const item = state.cart.at(i);
+        totalPrice += getProductPrice(item);
+      }
+      return totalPrice;
     },
   },
   mutations: {
