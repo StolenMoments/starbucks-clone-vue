91c6ccd05cdf1207b691ad5edcd5bd5c962156d2
lee <unknown9732@naver.com>
2022-03-13 14:06:29 +0900

    refactor(LoginForm) : 로그인 실패 모달 분리

--- src/components/Login/LoginForm.vue
+++ src/components/Login/LoginForm.vue
@@ -34,23 +34,18 @@
       <span data-test="show-password">{{ password }}</span>
     </section>
   </form>
-  <section v-if="isLoginFail" class="flex items-center justify-center">
-    <section class="ModalBlock" @click="toggleModal" @keydown="toggleModal">
-    </section>
-    <section class="ModalContent">
-      <div class="text-3xl font-bold">로그인에 실패했습니다</div>
-      <div class="mt-4 text-md font-bold">화면 클릭, 스페이스바 / 엔터키를 누르면 창이 닫힙니다.</div>
-    </section>
-  </section>
+  <LoginFailModal v-if="isLoginFail" v-on:onToggleModal="toggleModal"/>
 </template>
 
 <script>
 import RepositoryFactory from '@/components/Client/RepositoryFactory';
+import LoginFailModal from '@/components/Modal/LoginFailModal.vue';
 
 const userRepository = RepositoryFactory.get('user');
 
 export default {
   name: 'LoginForm',
+  components: { LoginFailModal },
   data() {
     return {
       username: '',
@@ -93,20 +88,4 @@ export default {
   @apply h-12 w-24 border-green-400 border-2 ml-1 px-4 py-2 mt-1 mb-1 font-bold rounded-2xl
   hover:bg-green-200
 }
-
-.ModalBlock {
-  @apply fixed h-full w-full flex justify-center items-center top-0 bg-gray-200 opacity-50 z-0
-}
-
-.ModalContent {
-  @apply absolute inline-block top-72 h-1/5 w-1/2 flex flex-col justify-center
-  items-center z-50 bg-white text-center
-}
-
-@media (max-width: 770px) {
-  .ModalContent {
-    @apply w-full
-  }
-}
-
 </style>
---
+++ src/components/Modal/LoginFailModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <section @click="this.$emit('onToggleModal')" @keydown="this.$emit('onToggleModal')">
+    <section class="ModalBlock">
+    </section>
+    <section class="ModalContent">
+      <div class="text-3xl font-bold">로그인에 실패했습니다</div>
+      <div class="mt-4 text-md font-bold">화면 클릭, 스페이스바 / 엔터키를 누르면 창이 닫힙니다.</div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LoginFailModal',
+};
+</script>
+
+<style scoped>
+.ModalBlock {
+  @apply fixed h-full w-full flex justify-center items-center top-0 bg-gray-200 opacity-50
+}
+
+.ModalContent {
+  @apply absolute inline-block top-72 h-1/5 w-1/2 flex flex-col justify-center
+  items-center bg-white text-center
+}
+
+@media (max-width: 770px) {
+  .ModalContent {
+    @apply w-full
+  }
+}
+</style>
