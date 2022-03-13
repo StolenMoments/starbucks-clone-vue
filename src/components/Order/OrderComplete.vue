2beed36f9b8fca8d44b6ef1ccc5f4de1e0cc3074
lee <unknown9732@naver.com>
2022-03-14 03:19:32 +0900

    feat(OrderComplete) : 결제 완료 화면 구현

---
+++ src/components/Order/OrderComplete.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="absolute flex flex-col h-full w-full items-center justify-center">
+    <div class="text-5xl font-bold">결제 성공</div>
+    <button class="mt-4 text-3xl underline text-blue-700" @click="goHome">홈으로 이동</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'OrderComplete',
+  methods: {
+    goHome() {
+      this.$router.push('/');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
