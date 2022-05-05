397639d0b638d4fd0e7aafdb7f4a1d423c8bd496
lee <unknown9732@naver.com>
2022-05-05 12:05:05 +0900

    design(LoginForm) : change modal style, message

--- src/components/Modal/LoginFailModal.vue
+++ src/components/Modal/LoginFailModal.vue
@@ -4,7 +4,7 @@
     </section>
     <section class="ModalContent">
       <div class="text-3xl font-bold">로그인에 실패했습니다</div>
-      <div class="mt-4 text-md font-bold">화면 클릭, 스페이스바 / 엔터키를 누르면 창이 닫힙니다.</div>
+      <div class="mt-4 text-md font-bold">화면을 클릭하면 창이 닫힙니다.</div>
     </section>
   </section>
 </template>
@@ -21,7 +21,8 @@ export default {
 }
 
 .ModalContent {
-  @apply absolute inline-block top-72 h-1/5 w-1/2 flex flex-col justify-center
+  @apply absolute inline-block top-0 left-0 right-0 bottom-0 m-auto
+  h-1/5 w-1/2 flex flex-col justify-center
   items-center bg-white text-center
 }
 
